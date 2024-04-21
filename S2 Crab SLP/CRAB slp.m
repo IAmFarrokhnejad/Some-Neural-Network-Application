39e5f8818410c1828b96b325e53cb213afcb01b3
Morteza Farrokhnejad <152209810+IAmFarrokhnejad@users.noreply.github.com>
2024-04-22 02:38:27 +0300

    Add files via upload
    Crab SLP training and testing

---
+++ S2 Crab SLP/CRAB slp.m
@@ -0,0 +1,22 @@
+#This code performs several tasks related to training and testing a single layer perceptron neural network using data from the CSV file 'crabs.csv'
+
+
+
+M = csvread('crabs.csv')
+M = readtable('crabs.csv')
+net=perceptron;
+view(net)
+net=newp(X,T);
+Xtr=X(:,1:170);
+Ttr=T(:,1:170);
+Xts=X(:,171:200);
+Tts=T(:,171:200);
+net=train(net,Xtr,Ttr);
+Ytr=net(Xtr)
+plotconfusion(Ttr,Ytr)
+Yts=net(Xts)
+plotconfusion(Tts,Yts)
+[c,cm] = confusion(Tts,Yts)
+net.trainParam.epochs=2000;
+net.trainParam.goal=2e-2;
+net.trainParam.lr=0.01;
